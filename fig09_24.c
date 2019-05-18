fa56592b25301ea6f0add47c2e2cd0d2eb3ac046
jietian20091231 <jietian20091231@gmail.com>
2019-05-18 23:59:18 +0800

    Character 9 Formatting Input and Output

--- fig09_24.c
+++ fig09_24.c
@@ -11,15 +11,13 @@ int main( void )
   int minute;
   int second;
 
-  char time_s[ 20 ] = { 0 };
-  
-  printf( "Enter a data in the form mm-dd-yyyy:" );
+  printf( "Enter a data in the form mm-dd-yyyy:\n" );
   scanf( "%d%*c%d%*c%d%*c%d%*c%d%*c%d", &day, &month, &year,
 	 &hour, &minute, &second );
 
   printf( "day = %d, month = %d, year = %d,"
 	  "hour = %d, minute = %d, second = %d\n",
-	  day, month, year + 1900, hour, minute, second );
+	  day, month, year, hour, minute, second );
 
   return 0;
 }
